e6ce85874dad7b9f80bd061d598292318a0ef00c
jetejetteraiASAP <situmonpainjetejetterai@gmail.com>
2021-10-29 14:41:12 +0530

    chore: commented function

--- draft3.sh
+++ draft3.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env sh
 
-leftover() {
-    git status -sb | sed "s/#.*//; s/M//; s/.*\s//; /^$/d"
-}
+# leftover() {
+    # git status -sb | sed "s/#.*//; s/M//; s/.*\s//; /^$/d"
+# }
+# 
+# leftover | nl -s: | sed -e "s/.*\s//"
+# 
+# printf "\nGive 3 numbers: "
+# read -r num
+# for n in ${num}
+# do
+    # temp=$(leftover | sed -n "${n}p")
+    # git add "${temp}"
+# done
 
-leftover | nl -s: | sed -e "s/.*\s//"
-
-printf "\nGive 3 numbers: "
-read -r num
-for n in ${num}
-do
-    temp=$(leftover | sed -n "${n}p")
-    git add "${temp}"
-done
+echo "find . -type f -name '"*"${1}"*\'""
